378fd6d67ff43284c0165970a869dd3315dfd5e2
tsubokura <repeat2639@gmail.com>
2022-11-22 16:11:02 +0900

    views.py fix

--- app/views.py
+++ app/views.py
@@ -131,7 +131,7 @@ def work_page6(request):
     params = {
         'name':'MENS MART',
         'msg':'Planning / Design / HTML,CSS,JavaScript,Python /',
-        'link':'https://mensmart-main7295-lowlands.herokuapp.com/',
+        'link':'https://django-ecsite6.onrender.com',
         'info':'メンズの洋服を取り揃えた"MENS MART"のWEBサイトは、男性ユーザー向けにスタイリッシュでシンプルな作りに仕上げています。また、白と黒を使い分けることで商品を際立たせています。ECサイト仕様に多様な機能を追加し、決済システムを導入しています。',
         'image_sp':'work/sp/image_sp_06.png',
         'image01':'work/page06/image01.png',
